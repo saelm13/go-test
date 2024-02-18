0b075931d7b5ebec9a93564f79cfe3e6702f4a31
saelm13 <160319674+saelm13@users.noreply.github.com>
2024-02-18 18:17:55 +0900

    Add files via upload

---
+++ study/1week/1week.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 10                    //변수 선언
+	var msg string = "Hello Variable" //msg 변수선언
+
+	a = 20
+	msg = "Good morning" // msg 값 변경
+	fmt.Println(msg, a)  //msg와 a값 출력
+}
